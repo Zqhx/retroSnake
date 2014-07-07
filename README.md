c09d29a6c87c3acab4d7d7e8ea77a634b958db82
Zqhx <arron.poucher@gmail.com>
2014-07-07 07:22:04 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+retroSnake
+==========
+
+PyGame meets vector graphics meets poor API design.
+
+The retroSnake.py file is the actual library code.
+
+The main.py file is currently a small test case showcasing two sprites,
+a line and a box that can be controlled independantly with the arrow
+keys and WASD respectively.
+
+It is my hope that with a little work this library could be used to
+implement some retro-style games such as space invaders or asteroids
+without having to rely upon bitmapped sprites.
---
+++ main.py
@@ -0,0 +1,81 @@
+import retroSnake
+from retroSnake import pygame
+
+
+def main():
+    if not retroSnake.init():
+        print "Error: Could not initialise retroSnake."
+        raise SystemExit
+    display = retroSnake.setupDisplay(640, 480, "Asteroids")
+
+    world = retroSnake.World()
+    camera = retroSnake.Camera(world)
+    camera.applyMatrix(retroSnake.newTranslateMatrix(320, 240))
+
+    line = retroSnake.Sprite([retroSnake.Line(
+        retroSnake.Vector(-20, -20),
+        retroSnake.Vector(20, 20))])
+    box = retroSnake.Sprite([
+        retroSnake.Line(
+            retroSnake.Vector(-20, -20),
+            retroSnake.Vector(20, -20),
+            ),
+        retroSnake.Line(
+            retroSnake.Vector(20, -20),
+            retroSnake.Vector(20, 20),
+            ),
+        retroSnake.Line(
+            retroSnake.Vector(20, 20),
+            retroSnake.Vector(-20, 20),
+            ),
+        retroSnake.Line(
+            retroSnake.Vector(-20, 20),
+            retroSnake.Vector(-20, -20),
+            )
+        ])
+    world.addSprite(line)
+    world.addSprite(box)
+
+    clock = pygame.time.Clock()
+
+    while True:
+        pygame.display.set_caption("FPS: %d" % (clock.get_fps(),))
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_i:
+                camera.applyMatrix(retroSnake.newScaleMatrix(2))
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+                camera.applyMatrix(retroSnake.newScaleMatrix(.5))
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            line.applyWorldMatrix(retroSnake.newTranslateMatrix(5, 0))
+        if keys[pygame.K_LEFT]:
+            line.applyWorldMatrix(retroSnake.newTranslateMatrix(-5, 0))
+        if keys[pygame.K_UP]:
+            line.applyWorldMatrix(retroSnake.newTranslateMatrix(0, -5))
+        if keys[pygame.K_DOWN]:
+            line.applyWorldMatrix(retroSnake.newTranslateMatrix(0, 5))
+
+        if keys[pygame.K_d]:
+            box.applyWorldMatrix(retroSnake.newTranslateMatrix(5, 0))
+        if keys[pygame.K_a]:
+            box.applyWorldMatrix(retroSnake.newTranslateMatrix(-5, 0))
+        if keys[pygame.K_w]:
+            box.applyWorldMatrix(retroSnake.newTranslateMatrix(0, -5))
+        if keys[pygame.K_s]:
+            box.applyWorldMatrix(retroSnake.newTranslateMatrix(0, 5))
+
+        box.applyLocalMatrix(retroSnake.newRotateMatrix(.01))
+        line.applyLocalMatrix(retroSnake.newRotateMatrix(-.01))
+
+        display.fill((0, 0, 0))
+        camera.draw()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
